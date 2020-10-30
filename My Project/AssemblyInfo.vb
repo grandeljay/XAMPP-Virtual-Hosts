da4b4f82045cb338287e3d86551dcc0820136214
Jay <grandel@posteo.de>
2020-10-30 08:41:12 +0100

    Update AssemblyInfo.vb

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -9,10 +9,10 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("XAMPP Virtual Hosts")>
-<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyDescription("Create and manage Virtual Hosts for XAMPP.")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("XAMPP Virtual Hosts")>
-<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyCopyright("Copyright © Jay 2020")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("0.0.1.0")>
+<Assembly: AssemblyFileVersion("0.0.1.0")>
